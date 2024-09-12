ca2be9fc6e92931c50d7d2afde001d9d8fd18a0b
lee-dong-chan <dsongc3082@naver.com>
2024-09-12 13:50:27 +0900

    edit git ignore

--- backend/src/middleware/logcheck/logcheck.middleware.ts
+++ backend/src/middleware/logcheck/logcheck.middleware.ts
@@ -6,10 +6,8 @@ export class LogcheckMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     if (req.session.user) {
       req.body.user = true;
-      console.log('m', true);
     } else {
       req.body.user = false;
-      console.log('m', false);
     }
 
     next();
